20e5a3211f0adb418c484d41d812380595026b84
Đặng Lê Đăng Khoa <58388376+danglekhoa1912@users.noreply.github.com>
2022-05-19 02:03:04 +0700

    add splash

--- App.js
+++ App.js
@@ -7,16 +7,21 @@
  */
 
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import RoorNavigation from './src/navigation/RoorNavigation';
 import store from './src/redux/roorStore';
 import {ApplicationProvider} from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
+import SplashScreen from 'react-native-splash-screen';
 
 import {navigationRef} from './src/navigation/NavigationWithoutProp';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <ApplicationProvider {...eva} theme={eva.light}>
